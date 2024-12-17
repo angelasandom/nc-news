554442db44950edc234e0b4433920bb36b4e136a
angelasandom <angelasandom@gmail.com>
2024-12-17 13:54:01 +0000

    complete display article by id task 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
+import { Route, Routes } from 'react-router'
 import Header from './components/Header'
 import ArticlesList from './components/ArticlesList'
+import ArticlePage from './components/ArticlePage'
 
 function App() {
 
   return (
     <>
       <Header />
-      <ArticlesList />
-      
+      <Routes>
+        <Route path="/articles" element={<ArticlesList/>}/>
+        <Route path="/articles/:article_id" element={<ArticlePage/>}/>
+      </Routes>
     </>
   )
 }
